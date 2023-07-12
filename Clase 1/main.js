d9eda677bf8ca41aa3acad46f576fb00b0029e90
andresf2448 <andresf2448@gmail.com>
2023-07-12 09:22:41 -0500

    Clase 1

---
+++ Clase 1/main.js
@@ -0,0 +1,44 @@
+// comentario de linea
+
+/* 
+comentario
+de
+bloque
+*/
+
+//forma recomendada para declarar variables
+// let edad = 15;
+// let nombre = "andres";
+// nombre = "juan";
+
+//forma no recomendada para declarar variables
+// var numero = 34;
+
+// constantes
+// const CONSTANTE = 5;
+
+// camelCase  =>  nombreGrandeBonito
+// snake_case =>  nombre_grande_bonito
+// PascalCase =>  NombreGrandeBonito
+
+// let numeroUno = 5;
+// let numeroDos = 10;
+
+// let resultadoSuma = numeroUno + numeroDos;
+// let resultadoResta = numeroUno - numeroDos;
+// let resultadoMultiplicacion = numeroUno * numeroDos;
+// let resultadoDivision = numeroUno / numeroDos;
+
+// console.log("Resultado suma", resultadoSuma);
+// console.log("Resultado resta", resultadoResta);
+// console.log("Resultado multiplicacion", resultadoMultiplicacion);
+// console.log("Resultado division", resultadoDivision);
+
+// let saludo = "Hola";
+// let nombre = "Camila";
+// let saludoCompleto = saludo + " " + nombre;
+
+// console.log(saludoCompleto);
+
+let nombre = prompt("Ingrese su nombre");
+alert(nombre);
