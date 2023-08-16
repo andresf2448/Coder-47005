c8f60ff0d0434170bf9354cc95d625b8fb54d3d9
andresf2448 <andresf2448@gmail.com>
2023-08-16 08:34:25 -0500

    Clase 11

---
+++ Clase 11/main.js
@@ -0,0 +1,49 @@
+//LOCALSTORAGE
+// localStorage.setItem("nombre", "andres"); // me permite ingresar datos al storage
+// localStorage.setItem("valor", true);
+// localStorage.setItem("numero", "5")
+
+// let nombre = localStorage.getItem("nombre"); // me trae el valor de un elemento del storage dada su clave
+// console.log(nombre);
+
+// let valor = localStorage.getItem("valor");
+// console.log(valor);
+
+// let numero = localStorage.getItem("numero");
+// console.log(numero);
+
+// for (let i = 0; i < localStorage.length; i++) {
+//   let clave = localStorage.key(i);
+
+//   console.log("clave => ",clave);
+//   console.log("valor obtenido => ", localStorage.getItem(clave));
+// }
+
+//ELIMINACIÓN EN EL STORAGE
+// localStorage.removeItem("nombre"); //elimina el item con clave nombre
+// localStorage.clear() //elimina todo el storage
+
+//SESSIONSTORAGE
+// sessionStorage.setItem("nombre", "andres"); // me permite ingresar datos al storage
+// sessionStorage.setItem("valor", true);
+// sessionStorage.setItem("numero", "5");
+
+// let nombre = sessionStorage.getItem("nombre"); // me trae el valor de un elemento del storage dada su clave
+// console.log(nombre);
+
+// let valor = sessionStorage.getItem("valor");
+// console.log(valor);
+
+// let numero = sessionStorage.getItem("numero");
+// console.log(numero);
+
+// for (let i = 0; i < sessionStorage.length; i++) {
+//   let clave = sessionStorage.key(i);
+
+//   console.log("clave => ",clave);
+//   console.log("valor obtenido => ", sessionStorage.getItem(clave));
+// }
+
+//ELIMINACIÓN EN EL STORAGE
+// sessionStorage.removeItem("nombre"); //elimina el item con clave nombre
+// sessionStorage.clear() //elimina todo el storage
